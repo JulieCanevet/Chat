516295a2a582a45755aeaca27ddc5d768ae4970b
Julie.Canevet <julie.canevet@hotmail.fr>
2017-07-21 13:41:50 +0200

    recuperation des premieres lettres

--- js/main.js
+++ js/main.js
@@ -5,14 +5,32 @@ var premierCaractere = pseudo.charAt(0);
 // récuperer la 1ere lettre du pseudo
 $('#you').html(premierCaractere.toUpperCase());
 
-$('#envoi').click(function() {
-	var message = $('#message').val();
-	$('.Y p').html(message);
-	$('#message').html();
-	setTimeout(function(){
-		$("div").addClass("R");
-	}, 2000);
+$('#message').keypress(function(e) {    
+   if(e.keyCode == 13) { // KeyCode de la touche entrée
+         envoyer(); 
+ }
 });
 
+
+$('#envoi').click(envoyer);
+
+	function envoyer() {
+	var message = $('#message').val(); // Recupere le message du textarea
+	$('#message').val(""); //Vide le textarea
+	$("div").last().after("<div class='msg Y'><h3 class='oui'></h3><p></p></div>");
+	$('.Y p').last().html(message);
+	$('.oui').html(premierCaractere.toUpperCase());
+ 
+ // Reponse de rebecca
+ setTimeout(function(){
+    $(".Y:last").after("<div class='msg R'><h3>R</h3><p>Ca va ?</p></div>");
+   	var scroll = document.getElementById('chat');
+	scroll.scrollTop = scroll.scrollHeight;
+ }, 2000);
+
+  	var scroll = document.getElementById('chat');
+	scroll.scrollTop = scroll.scrollHeight;
+
+};
 
 
