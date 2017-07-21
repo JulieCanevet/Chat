dd2b26895dabbec89e10de6e46ca9db5fec85302
Julie.Canevet <julie.canevet@hotmail.fr>
2017-07-21 14:44:50 +0200

    ajout de preventDefaut envoi enter

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
 var pseudo = prompt ('Quel est ton nom ?');
 var premierCaractere = pseudo.charAt(0);
-
+var reponses = ["ca va ?", "oui", "merci", "je ne sais pas et toi ?"];
+var x = 0;
 
 // récuperer la 1ere lettre du pseudo
 $('#you').html(premierCaractere.toUpperCase());
 
+// cliquer sur la touche entrer pour envoyer les msg
 $('#message').keypress(function(e) {    
-   if(e.keyCode == 13) { // KeyCode de la touche entrée
+   if(e.keyCode == 13) { 
+   e.preventDefault();// KeyCode de la touche entrée
          envoyer(); 
+         var message = $('#message').val("");
  }
 });
 
-
+// Envoyer avec le click du bouton (smartphone)
 $('#envoi').click(envoyer);
 
+//
 	function envoyer() {
 	var message = $('#message').val(); // Recupere le message du textarea
-	$('#message').val(""); //Vide le textarea
-	$("div").last().after("<div class='msg Y'><h3 class='oui'></h3><p></p></div>");
+	//Vide le textarea
+	$("div").last().after("<div class='msg Y'><h3 class='you'></h3><p></p></div>");
 	$('.Y p').last().html(message);
-	$('.oui').html(premierCaractere.toUpperCase());
+	$('.you').html(premierCaractere.toUpperCase());
+ 	
+ 	var scroll = document.getElementById('chat');
+	scroll.scrollTop = scroll.scrollHeight;
+	var message = $('textarea').val("");
  
  // Reponse de rebecca
  setTimeout(function(){
-    $(".Y:last").after("<div class='msg R'><h3>R</h3><p>Ca va ?</p></div>");
+
+    $(".Y:last").after("<div class='msg R'><h3>R</h3><p class='answer'></p></div>");
+   	
+   	$('.answer:last').html(reponses[x]);
+   	x++;
    	var scroll = document.getElementById('chat');
 	scroll.scrollTop = scroll.scrollHeight;
- }, 2000);
 
-  	var scroll = document.getElementById('chat');
-	scroll.scrollTop = scroll.scrollHeight;
+ }, 2000);
 
 };
 
